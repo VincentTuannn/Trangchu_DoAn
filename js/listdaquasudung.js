69c832cb3de332eb359c3482a6d2b7a49d9c03a9
PhamThanhSu <147363895+PhamThanhSu@users.noreply.github.com>
2023-11-07 14:42:32 +0700

    Add files via upload

---
+++ js/listdaquasudung.js
@@ -0,0 +1,50 @@
+
+// Laptop
+var daquasudung = document.getElementById('daquasudung');
+var menudqsd = document.getElementById('list-daquasudung');
+var activebanner = document.getElementById('menu-active-tm');
+
+daquasudung.addEventListener("mouseenter", function() {
+        menudqsd.style.display = "block";
+        activebanner.style.display="none";
+});
+
+menudqsd.addEventListener("mouseenter", function() {
+    menudqsd.style.display = "block";
+    activebanner.style.display="none";
+});
+
+daquasudung.addEventListener("mouseleave", function() {
+    menudqsd.style.display="none"; 
+    activebanner.style.display="block";
+});
+
+menudqsd.addEventListener("mouseleave", function() {
+    menudqsd.style.display = "none";
+    activebanner.style.display="block";
+});
+
+//Nhu cầu sử dụng
+var daquasudung = document.getElementById('daquasudung');
+var menudqsd = document.getElementById('list-daquasudung');
+var activebanner = document.getElementById('menu-active-tm');
+
+daquasudung.addEventListener("mouseenter", function() {
+        menudqsd.style.display = "block";
+        activebanner.style.display="none";
+});
+
+menudqsd.addEventListener("mouseenter", function() {
+    menudqsd.style.display = "block";
+    activebanner.style.display="none";
+});
+
+daquasudung.addEventListener("mouseleave", function() {
+    menudqsd.style.display="none"; 
+    activebanner.style.display="block";
+});
+
+menudqsd.addEventListener("mouseleave", function() {
+    menudqsd.style.display = "none";
+    activebanner.style.display="block";
+});
--- js/login_header.js
+++ js/login_header.js
@@ -7,13 +7,29 @@ var loginButton = document.getElementById('button-login');  //Nút đăng nhập
 var isLoginPopupOpen;
 
 // Thêm sự kiện click cho nút "Đăng nhập"
-loginButton.addEventListener('click', function() {
+loginButton.addEventListener('click', function(event) {
+    event.stopPropagation();
     if (!isLoginPopupOpen) {
         // Hiển thị cửa sổ nếu nó đang ẩn
         loginPopup.style.display = 'block';
         loginContent.classList.add('active');
         isLoginPopupOpen = true;
     } else {
+        // Ẩn cửa sổ nếu nó đang hiển thị
+        loginPopup.style.display = 'none';
+        loginContent.classList.remove('active');
+        isLoginPopupOpen = false;
+    }
+    if(isLoginPopupCartOpen){
+        loginPopupCart.style.display = 'none';
+        loginContentCart.classList.remove('active');
+        isLoginPopupCartOpen = false;
+    }
+});
+
+// Sự kiện click bất kỳ nơi nào trên trang để ẩn cửa sổ
+document.body.addEventListener('click', function(event) {
+    if (isLoginPopupOpen && !loginContent.contains(event.target)){
         // Ẩn cửa sổ nếu nó đang hiển thị
         loginPopup.style.display = 'none';
         loginContent.classList.remove('active');
