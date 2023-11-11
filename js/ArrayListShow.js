9464a9ba0209a54639f887434dabd5c2c1e70c77
PhamThanhSu <147363895+PhamThanhSu@users.noreply.github.com>
2023-11-11 18:00:30 +0700

    Add files via upload

--- js/ArrayListShow.js
+++ js/ArrayListShow.js
@@ -2,193 +2,209 @@ const ArrayListProducts = [
     {
         img: "./image/selling-products.webp",
         name: "Laptop LG Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "LG",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "25490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop ASUS Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "ASUS",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "5490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop DELL Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "DELL",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "25490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop HP Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "HP",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "25490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop APPLE Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "APPLE",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "25490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop VIVOZENBOOK Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "VIVOZENBOOK",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "25490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop GIGABYTE Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "GIGABYTE",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "5490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop LG Gram 2022 ",
+        thuonghieu: "LG",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "25490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop ACER Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "ACER",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "25490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop ASUS TUF DASH Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "ASUS",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "25490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop LG Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "LG",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "2490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop LG Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "LG",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "9490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop ASUS Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "ASUS",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "35490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop DELL Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "DELL",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "15490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop HP Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "HP",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "45490000",
         labelonsale: "-21%"
     },
     {
         img: "./image/selling-products.webp",
         name: "Laptop APPLE Gram 2023 14Z90R GAH53A5",
+        thuonghieu: "APPLE",
         cpu: "CPU 1",
         vga: "VGA 1",
         ram: "16 GB",
         ssd: "256 GB",
         lcd: "14 inch FHD+ IPS",
         pricecompare: "32.490.000₫",
-        pricehighlight: "25.490.000₫",
+        pricehighlight: "17490000",
         labelonsale: "-21%"
     }
 ];
@@ -200,43 +216,67 @@ function handleSearchFileresult() {
     return false; // Ngăn chặn việc gửi biểu mẫu
 }
 
-function ListSellingProducts(products) {
+function ListSellingProducts(filterthuonghieu = [], filtergiaban = []) {
     let s = `<div class="item-selling-products" id="item-selling-products">`;
+    for (let i = 0; i < ArrayListProducts.length; i++) {
+        const product = ArrayListProducts[i];
+        var anh = product.img;
+        var thuonghieu = product.thuonghieu;
+        var name = product.name;
+        var cpu = product.cpu;
+        var ram = product.ram;
+        var ssd = product.ssd;
+        var vga = product.vga;
+        var lcd = product.lcd;
+        var pricecompare = product.pricecompare;
+        var pricehighlight = product.pricehighlight;
+        var labelonsale = product.labelonsale;
 
-    for (let i = 0; i < products.length; i++) {
-        const product = products[i];
+        console.log('filter.thuonghieu.length');
+        if (filterthuonghieu.length > 0) {
+            if (filterthuonghieu.includes(thuonghieu) == false)
+                continue;
+        }
+
+        if (filtergiaban.length > 0) {
+            if (pricehighlight < 10000000 && !filtergiaban.includes('1')) continue;
+            if (pricehighlight >= 10000000 && pricehighlight <= 15000000 && !filtergiaban.includes('2')) continue;
+            if (pricehighlight > 15000000 && pricehighlight <= 25000000 && !filtergiaban.includes('3')) continue;
+            if (pricehighlight > 25000000 && !filtergiaban.includes('4')) continue;
+        }
+
         s += `
             <div class="item-show">
                 <a href="#">
-                    <img src="${product.img}" width="100%"/>
+                    <img src="${anh}" width="100%"/>
                 </a>
                 <div class="detail">
                     <h3 class="name-product">
-                        <a id="name" href="#" title="${product.name}" tabindex="0">${product.name}</a>
+                        <a id="name" href="#" title="${name}" tabindex="0">${name}</a>
                     </h3>
                     <div class="technical">
                         <div class="cpu">
-                            <span>${product.cpu}</span>
+                            <span>${cpu}</span>
                         </div>
                         <div class "vga">
-                            <span>${product.vga}</span>
+                            <span>${vga}</span>
                         </div>
                         <div class="ram">
-                            <span>${product.ram}</span>
+                            <span>${ram}</span>
                         </div>
                         <div class="ssd">
-                            <span>${product.ssd}</span>
+                            <span>${ssd}</span>
                         </div>
                         <div class="lcd">
-                            <span>${product.lcd}</span>
+                            <span>${lcd}</span>
                         </div>
                         <div class="price">
                             <div class="price--compare">
-                                <del>${product.pricecompare}</del>
+                                <del>${pricecompare}</del>
                             </div>
                             <div class="price--default">
-                                <span class="price--highlight">${product.pricehighlight}</span>
-                                <span class="label--on-sale">${product.labelonsale}</span>
+                                <span class="pricehighlight">${pricehighlight}</span>
+                                <span class="label--on-sale">${labelonsale}</span>
                             </div>
                         </div>
                     </div>
@@ -263,7 +303,61 @@ if (searchTerm) {
 }
 
 
+function filteritem() {
+    var arrthuonghieu = document.getElementsByClassName("thuonghieu");
+    var filterthuonghieu = [];
+    for (var i = 0; i < arrthuonghieu.length; i++) {
+        if (arrthuonghieu[i].checked) filterthuonghieu.push(arrthuonghieu[i].value);
+    }
+
+    console.log('filterthuonghieu');
+   
+    var arrgiaban = document.getElementsByClassName("gia");
+    var filtergiaban = [];
+    for(var i = 0; i< arrgiaban.length; i++){
+        if(arrgiaban[i].checked) filtergiaban.push(arrgiaban[i].value);
+    }
+    ListSellingProducts(filterthuonghieu,filtergiaban);  
+}
 
 
 
 
+
+/*
+function filteritem() {
+    var arrthuonghieu = document.getElementsByClassName("thuonghieu");
+    var filterthuonghieu = [];
+    for (var i = 0; i < arrthuonghieu.length; i++) {
+        if (arrthuonghieu[i].checked) filterthuonghieu.push(arrthuonghieu[i].value);
+    }
+
+    var arrgiaban = document.getElementsByClassName("gia");
+    var filtergiaban = [];
+    for (var i = 0; i < arrgiaban.length; i++) {
+        if (arrgiaban[i].checked) filtergiaban.push(arrgiaban[i].value);
+    }
+
+    ListSellingProducts(filterthuonghieu, filtergiaban);
+}
+
+/* Lọc sản phẩm theo thương hiệu và giá
+function ListSellingProducts(filterthuonghieu = [], filtergiaban = []) {
+    let s = `<div class="item-selling-products" id="item-selling-products">`;
+    for (let i = 0; i < ArrayListProducts.length; i++) {
+        const product = ArrayListProducts[i];
+        var thuonghieu = product.thuonghieu;
+        var pricehighlight = parseInt(product.pricehighlight);
+
+        if (filterthuonghieu.length > 0 && !filterthuonghieu.includes(thuonghieu)) {
+            continue;
+        }
+
+        if (filtergiaban.length > 0) {
+            if (pricehighlight < 10000000 && !filtergiaban.includes('1')) continue;
+            if (pricehighlight >= 10000000 && pricehighlight <= 15000000 && !filtergiaban.includes('2')) continue;
+            if (pricehighlight > 15000000 && pricehighlight <= 25000000 && !filtergiaban.includes('3')) continue;
+            if (pricehighlight > 25000000 && !filtergiaban.includes('4')) continue;
+        }
+    }
+ */
--- js/Arrayfilter.js
+++ js/Arrayfilter.js
@@ -1,138 +1,63 @@
-var ArrayListFilterThuonghieu = [
-    {filtername:" ASUS"},
-    {filtername:" DELL"}
-];
-var ArrayListFilterGia = [
-    {filtergia:"Dưới 10.000.000đ"},
-    {filtergia:"10.000.000đ - 15.000.000đ"},
-];
-var ArrayListFilterCPU = [
-    {filtercpu:"Dưới 10.000.000đ"},
-    {filtercpu:"10.000.000đ - 15.000.000đ"},
-];
-var ArrayListFilterVGA = [
-    {filtervga:"Dưới 10.000.000đ"},
-    {filtervga:"10.000.000đ - 15.000.000đ"},
-];
+function ListSellingProducts(ArrayListProducts) {
+    let s = `<div class="item-selling-products" id="item-selling-products">`;
+    for (let i = 0; i < ArrayListProducts.length; i++) {
+        const product = ArrayListProducts[i];
+        var anh = product.img;
+        var thuonghieu = product.thuonghieu;
+        var name = product.name
+        var cpu = product.cpu;
+        var ram = product.ram;
+        var ssd = product.ssd;
+        var vga = product.vga;
+        var lcd = product.lcd;
+        var pricecompare = product.pricecompare;
+        var pricehighlight = product.pricehighlight;
+        var labelonsale = product.labelonsale;
 
-
-
-var thuonghieu = document.getElementById('item-thuonghieu');
-thuonghieu.addEventListener('click', function(){
-    ListFilterThuonghieu();
-});
-// Gọi hàm để tạo danh sách khi trang được tải
-ListFilterThuonghieu();
-function ListFilterThuonghieu() {
-    let listfilter = '';
-    for (let i = 0; i < ArrayListFilterThuonghieu.length; i++) {
-        const filter = ArrayListFilterThuonghieu[i];
-        listfilter += `
-            <li>
-                <label id="label-item-search">
-                    <input type="checkbox" id="filtername">${filter.filtername}
-                </label>
-            </li>
-             `;
-    }
-    document.getElementById("group-filter-thuonghieu").innerHTML = listfilter;
-}
-
-
-
-
-var gia = document.getElementById('item-gia');
-gia.addEventListener('click', function(){
-    ListFilterGia();
-});
-// Gọi hàm để tạo danh sách khi trang được tải
-ListFilterGia();
-function ListFilterGia(){
-    let listfiltergia = '';
-    for (let i = 0; i < ArrayListFilterGia.length; i++) {
-        const filtergia = ArrayListFilterGia[i];
-        listfiltergia += `
-            <li>
-                <label>
-                    <input type="checkbox">
-                    <p class="filtergia">${filtergia.filtergia}</p>
-                </label>
-            </li>
-             `;
-    }
-    document.getElementById("group-filter-gia").innerHTML = listfiltergia;
-}
-
-
-
-
-var cpu = document.getElementById('item-cpu');
-cpu.addEventListener('click', function(){
-    ListFilteCPU();
-});
-// Gọi hàm để tạo danh sách khi trang được tải
-ListFilterCPU();
-function ListFilterCPU(){
-    let listfiltercpu = '';
-    for (let i = 0; i < ArrayListFilterCPU.length; i++) {
-        const filtercpu = ArrayListFilterCPU[i];
-        listfiltercpu += `
-            <li>
-                <label>
-                    <input type="checkbox">
-                    <p class="filtercpu">${filtercpu.filtercpu}</p>
-                </label>
-            </li>
-             `;
-    }
-    document.getElementById("group-filter-cpu").innerHTML = listfiltercpu;
-}
-
-
-
-var vga = document.getElementById('item-vga');
-vga.addEventListener('click', function(){
-    ListFilteVGA();
-});
-// Gọi hàm để tạo danh sách khi trang được tải
-ListFilterVGA();
-function ListFilterVGA(){
-    let listfiltervga = '';
-    for (let i = 0; i < ArrayListFilterVGA.length; i++) {
-        const filtervga = ArrayListFilterVGA[i];
-        listfiltervga += `
-            <li>
-                <label>
-                    <input type="checkbox">
-                    <p class="filtervga">${filtervga.filtervga}</p>
-                </label>
-            </li>
-             `;
-    }
-    document.getElementById("group-filter-vga").innerHTML = listfiltervga;
-}
-
-
-
-
-/* Hàm tìm kiếm xuất giá trị trong trang */ 
-function handleSearch() {
-    const text = document.getElementById('filtername');
-    const products = document.getElementsByClassName('name');
-    for (let i = 0; i < products.length; i++) {
-        const product = products[i];
-        const productName = product.textContent.toLowerCase();
-        const productItem = product.parentElement.parentElement.parentElement;
-        const searchResult = text.textContent;
-        if (productName.includes(searchResult.toLowerCase())) {
-            productItem.style.display = 'block';
-        } else {
-            productItem.style.display = 'none';
+        if(filterthuonghieu.length > 0){
+            if(filterthuonghieu.includes(thuonghieu) == false)
+            continue;
         }
+
+        s += `
+            <div class="item-show">
+                <a href="#">
+                    <img src="${anh}" width="100%"/>
+                </a>
+                <div class="detail">
+                    <h3 class="name-product">
+                        <a id="name" href="#" title="${name}" tabindex="0">${name}</a>
+                    </h3>
+                    <div class="technical">
+                        <div class="cpu">
+                            <span>${cpu}</span>
+                        </div>
+                        <div class "vga">
+                            <span>${vga}</span>
+                        </div>
+                        <div class="ram">
+                            <span>${ram}</span>
+                        </div>
+                        <div class="ssd">
+                            <span>${ssd}</span>
+                        </div>
+                        <div class="lcd">
+                            <span>${lcd}</span>
+                        </div>
+                        <div class="price">
+                            <div class="price--compare">
+                                <del>${pricecompare}</del>
+                            </div>
+                            <div class="price--default">
+                                <span class="price--highlight">${pricehighlight}</span>
+                                <span class="label--on-sale">${labelonsale}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
     }
 
-    return false; // Ngăn chặn việc gửi biểu mẫu
-}
-
-
-
+    s += `</div>`;
+    document.getElementById("item-selling-products").innerHTML = s;
+} 
